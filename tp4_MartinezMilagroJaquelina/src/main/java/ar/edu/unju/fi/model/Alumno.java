4149cd3716f5957507c3650842d8e5eb8a5a2435
mili-08 <milijmartinez@gmail.com>
2024-05-25 02:15:43 -0300

    Estilos en header, maquetado de todo el index y creacion de clases

--- tp4_MartinezMilagroJaquelina/src/main/java/ar/edu/unju/fi/model/Alumno.java
+++ tp4_MartinezMilagroJaquelina/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -1,5 +1,103 @@
 package ar.edu.unju.fi.model;
 
-public class Alumno {
+import java.time.LocalDate;
 
+public class Alumno {
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+	private LocalDate fechaNac;
+	private String domicilio;
+	private Integer lu;
+
+	public Alumno() {
+
+	}
+
+	public Alumno(String dni, String nombre, String apellido, String email, String telefono, LocalDate fechaNac,
+			String domicilio, Integer lu) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+		this.fechaNac = fechaNac;
+		this.domicilio = domicilio;
+		this.lu = lu;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public LocalDate getFechaNac() {
+		return fechaNac;
+	}
+
+	public void setFechaNac(LocalDate fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+
+	public String getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+
+	public Integer getLu() {
+		return lu;
+	}
+
+	public void setLu(Integer lu) {
+		this.lu = lu;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
+				+ ", telefono=" + telefono + ", fechaNac=" + fechaNac + ", domicilio=" + domicilio + ", lu=" + lu + "]";
+	}
+	
+	
 }
