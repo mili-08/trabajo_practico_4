6d6dad372d1b88d20bd41ecec8f0062f41db196f
mili-08 <milijmartinez@gmail.com>
2024-06-08 23:46:07 -0300

    Creación de la rama feature/alumno y de AlumnoController

---
+++ tp4_MartinezMilagroJaquelina/src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("alumno")
+
+public class AlumnoController {
+
+}
